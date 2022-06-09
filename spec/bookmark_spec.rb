7dff495302a42bb55ba54f17a8a0c176b74eb6cf
Joe McGarry <joewmcgarry@gmail.com>
2022-06-09 11:48:27 +0100

    Adds functionality to delete bookmarks

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -31,4 +31,12 @@ describe Bookmark do
       expect(bookmark.url).to eq 'http://www.example.org'
     end
   end
+
+  describe '.delete' do
+    it 'deletes a specific bookmark' do
+      bookmark = Bookmark.create(url: "http://www.bbc.co.uk", title: "BBC")
+      Bookmark.delete(id: bookmark.id)
+      expect(Bookmark.all.length).to eq 0
+    end
+  end
 end
